7c15da99574d00d5543e4696459e2ee1e64c9782
Adam <adam.urban@gmail.com>
2022-04-04 20:05:34 +0200

    feat(files): sync

--- scripts/lock.sh
+++ scripts/lock.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 set -e
 xset s off dpms 0 10 0
-i3lock --color=000000 --ignore-empty-password --show-failed-attempts --nofork
+i3lock -e -f -n -i ~/.config/i3/i3lock.png -c 000000
+# i3lock --color=000000 --ignore-empty-password --show-failed-attempts --nofork
 xset s off -dpms
