5a52aa4653fc9cde027d5f58497aafc1d1b992eb
Adam <adam.urban@gmail.com>
2022-06-20 19:13:36 +0200

    feat(scrips): update lock script

--- scripts/lock.sh
+++ scripts/lock.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -e
-xset s off dpms 0 10 0
+#xset s off dpms 0 10 0
+xset dpms force off
 i3lock -e -f -n -i ~/.config/i3/i3lock.png -c 000000
 # i3lock --color=000000 --ignore-empty-password --show-failed-attempts --nofork
-xset s off -dpms
+#xset s off -dpms
+#xset dpms force off
